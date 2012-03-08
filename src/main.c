f77d018b721db101b95a207519495ea70772613a
vadali <shlomivaknin@gmail.com>
2012-03-08 17:04:39 +0200

    play around with parsing

--- src/main.c
+++ src/main.c
@@ -35,99 +35,104 @@
 /* define our global matrices */
 Mat Mats[MAX_MATS];
 
+typedef enum {
+	immediate = 0,
+	direct,
+	label_with_index,
+	label_with_two_indices,
+	reg
+} addressingE;
+
+typedef struct {
+	addressingE kind;
+	void *d1,*d2,*d3; /* data holders according with kind */
+} addressing;
+
+#define TWO (A,B,code ){ \
+	addressing op1; \
+	addressing op2; \
+					\
+	A(&op1);		\
+	B(&p2);			\
+	code;			\
+}
+
+#define ERR( B , S ,code) { \
+	if (B) { \
+		printf("%s", S);\
+	} \
+	else \
+		code; \
+}
+
+#define OR l==NULL
+#define DO 1
+
+#define REG( B , r ,code) { \
+	 /* register */ \
+	int r = -1; \
+	if (B) { \
+		l = charIs(strip(p," "), 'r'); \
+			if (l!=NULL) { \
+				if (isDigit(l)) { \
+					r = 0; \
+					r = (char)*(l)-'0'; \
+				} \
+				else l = NULL; \
+			if (l!=NULL){ \
+				l++;\
+				p=l;\
+			}\
+		}\
+	}\
+	code; \
+}
+
+#define CONST(B, i, code) { \
+	int i = -1; \
+	if (B) { \
+		l = charIs(strip(p," "),'#');\
+		if (l!=NULL) { \
+			l = getInteger(strip(l," "),&i, &err); \
+			if (l!=NULL) { \
+				l++; \
+				p=l; \
+			} \
+		}\
+	}\
+	code;\
+}
+
+
+void printMe(int r, int i) {
+	if (r!=-1) {
+		printf("got register %i\n", r);
+		return;
+	}
+	if (i!=-1) {
+		printf("got constant %d\n", i);
+		return;
+	}
+	printf("Shouldn't get here, use ERR!");
+}
+
 int main(void) {
 
 	char line[1000];
 	int err = 0;
 	int f = 0;
 	char *l;
+	char *p;
 
-	/*
-	node* n;
-
-	int i = 42;
-
-	if (i-=2) {
-		printf("ass\n");
-	}
-
-	printf("%d\n", (i+=12)-(i+=67));
-
-
-	n = malloc(sizeof(node));
-	n->data = 4;
-	insertSorted(n);
-
-	n = malloc(sizeof(node));
-	n->data = 5;
-	insertSorted(n);
-
-	n = malloc(sizeof(node));
-	n->data = 2;
-	insertSorted(n);
-
-	n = malloc(sizeof(node));
-	n->data = 9;
-	insertSorted(n);
-
-	print();
-	printBackwards();
-
-	n = malloc(sizeof(node));
-	n->data = 6;
-	insertSorted(n);
-
-	n = malloc(sizeof(node));
-	n->data = 1;
-	insertSorted(n);
-
-	n = malloc(sizeof(node));
-	n->data = 16;
-	insertSorted(n);
-
-	print();
-	printBackwards();
-
-	freeList();
-	*/
 	/* endlessly read lines, passing them to our dsl.
 	 * since the code is so declarative, i see no reason to comment this file any more :) */
 	while (1) {
 		getline(line, sizeof(line));
-		l = line;
-		{
-			char *p = l;
-			{ /* register */
-				char r=-1;
-				l = charIs(strip(p," "), 'r');
-				if (l!=NULL) {
-					if (isDigit(l)){
-						r = (char)*l;
-					}
-					if (l!=NULL){
-						l++;
-						p=l;
-					}
-				}
-			}
-			if (l==NULL) {
-				int i=-1; /*constant*/
-				l = charIs(strip(p," "),'#');
-				if (l!=NULL) {
-					l = getInteger(strip(l," "),&i, &err);
-					if (l!=NULL){
-						l++;
-						p=l;
-					}
-				}
-			}
-			if (l==NULL) { /*label*/
+		p = line;
+		l=NULL;
 
-			}
+		REG(DO, r1, CONST(OR, c1, ERR(OR, "bad input\n", printMe(r1,c1))));
 
-
-			printf("%d\n", f);
-		}
 		/*
 		 PARSE(line)
 		 TRY(read_mat)
@@ -144,3 +149,51 @@ int main(void) {
 
 	return 0;
 }
+/*
+node* n;
+
+int i = 42;
+
+if (i-=2) {
+	printf("ass\n");
+}
+
+printf("%d\n", (i+=12)-(i+=67));
+
+
+n = malloc(sizeof(node));
+n->data = 4;
+insertSorted(n);
+
+n = malloc(sizeof(node));
+n->data = 5;
+insertSorted(n);
+
+n = malloc(sizeof(node));
+n->data = 2;
+insertSorted(n);
+
+n = malloc(sizeof(node));
+n->data = 9;
+insertSorted(n);
+
+print();
+printBackwards();
+
+n = malloc(sizeof(node));
+n->data = 6;
+insertSorted(n);
+
+n = malloc(sizeof(node));
+n->data = 1;
+insertSorted(n);
+
+n = malloc(sizeof(node));
+n->data = 16;
+insertSorted(n);
+
+print();
+printBackwards();
+
+freeList();
+*/
