e64e0cd5259d4d265fccacbd1c735f04e0dba4df
vadali <shlomivaknin@gmail.com>
2012-03-31 20:43:08 +0300

    plenty of documentations..

--- src/asmdsl.c
+++ src/asmdsl.c
@@ -2,12 +2,18 @@
  * asmDsl.c
  *
  *  Created on: Mar 8, 2012
- *      Author: shlomi
+ *      Author: shlomi.v
+ *
+ *  This file is the most interesting one!
+ *  if you run only the c preprocessor on this file, you will see that all the actual code of our app goes in here.
+ *
+ *  first, we will assign between a generating function and its defined command
+ *  and second, we will hook these assigned generators into the parsers, so once a parser succeeds, it will simply call one of these generating functions
  */
 
 #include "asmdsl.h"
 
-/* assign the functions that will actually generate the op codes for each command*/
+/* assign the functions that will actually generate the codes for each command*/
 ASSIGN2(mov, MOV)
 ASSIGN2(cmp, CMP)
 ASSIGN2(add, ADD)
@@ -30,27 +36,31 @@ ASSIGN0(stop, STOP)
 /* code in the table on page 33
  * this defines the parsers for each command, where at the end of each parser, we supply the function which
  * generates the actual op-codes (defined above) */
-CMD(cmp) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR5(REG, DIRECT, LABEL1, LABEL2, CONST),  GEN2(cmp))
-CMD(mov) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR4(REG, DIRECT, LABEL1, LABEL2), 		GEN2(mov))
-CMD(add) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR4(REG, DIRECT, LABEL1, LABEL2), 		GEN2(add))
-CMD(sub) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR4(REG, DIRECT, LABEL1, LABEL2), 		GEN2(sub))
-CMD(lea) TWO(OR3(DIRECT, LABEL1, LABEL2),             OR4(REG, DIRECT, LABEL1, LABEL2), 		GEN2(lea))
 
-CMD(prn) ONE(OR5(REG, DIRECT, CONST,  LABEL1, LABEL2),		GEN1(prn))
-CMD(not) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(not))
-CMD(clr) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(clr))
-CMD(inc) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(inc))
-CMD(dec) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(dec))
-CMD(jmp) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(jmp))
-CMD(bne) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(bne))
-CMD(red) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),				GEN1(red))
-CMD(jsr) ONE(DIRECT,										GEN1(jsr))
+/* lets take cmp as an example to explain each line:
+ *   cmp is a command that takes in two operands, the first is one of five (reg, const, ...), and the second is also one of five.
+ *   if this is the command you got, call generating function cmp (as defined in line ASSIGN2(cmp, CMP), which generates a cmp command with two operands.*/
+PARSER(cmp) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR5(REG, DIRECT, LABEL1, LABEL2, CONST),  	GEN2(cmp))
+PARSER(mov) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR4(REG, DIRECT, LABEL1, LABEL2), 			GEN2(mov))
+PARSER(add) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR4(REG, DIRECT, LABEL1, LABEL2), 			GEN2(add))
+PARSER(sub) TWO(OR5(REG, CONST, DIRECT, LABEL1, LABEL2), OR4(REG, DIRECT, LABEL1, LABEL2), 			GEN2(sub))
+PARSER(lea) TWO(OR3(DIRECT, LABEL1, LABEL2),             OR4(REG, DIRECT, LABEL1, LABEL2), 			GEN2(lea))
 
-CMD(rts)  NONE(GEN0(rts))
-CMD(stop) NONE(GEN0(stop))
+PARSER(prn) ONE(OR5(REG, DIRECT, CONST,  LABEL1, LABEL2),											GEN1(prn))
+PARSER(not) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(not))
+PARSER(clr) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(clr))
+PARSER(inc) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(inc))
+PARSER(dec) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(dec))
+PARSER(jmp) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(jmp))
+PARSER(bne) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(bne))
+PARSER(red) ONE(OR4(REG, DIRECT, LABEL1, LABEL2),													GEN1(red))
+PARSER(jsr) ONE(DIRECT,																				GEN1(jsr))
 
-CMD(entry) ONE(DIRECT, GEN1(entry))
-CMD(_extern) ONE(DIRECT, GEN1(extern_))
+PARSER(rts)  NONE(GEN0(rts))
+PARSER(stop) NONE(GEN0(stop))
 
-CMD(data) MANY_NUMBERS(CALL_DATA(data))
-CMD(string) STRING(CALL_STRING(string))
+PARSER(entry) ONE(DIRECT, GEN1(entry))
+PARSER(_extern) ONE(DIRECT, GEN1(extern_))
+
+PARSER(data) MANY_NUMBERS(CALL_DATA(data))
+PARSER(string) STRING(CALL_STRING(string))
--- src/asmdsl.h
+++ src/asmdsl.h
@@ -2,7 +2,11 @@
  * asdDsl.h
  *
  *  Created on: Mar 8, 2012
- *      Author: shlomi
+ *      Author: shlomi.v
+ *      Author: Tal.a
+ *
+ *  This file defines all the DSL constructs used to create the actual parser and code generator for our assembly language.
+ *  check out asmdsl.c to see the usage.
  */
 
 #ifndef ASMDSL_H_
@@ -14,6 +18,7 @@
 #include "lists.h"
 #include "gendsl.h"
 
+/* A helper macro for defining commands of various arities*/
 #define DO_(c) \
 		int count=c;		 \
 		Operand a[c] ;		    \
@@ -22,15 +27,24 @@
 		val.kind = none; \
 		for (i=0;i<count;i++) { a[i].kind=none;}
 
+/* a helper macro for finishing an OR macro*/
 #define FIN_OR(code) \
 		for (i=0;val.kind==none&&i<count;i++) {val=a[i];} \
 		if (val.kind != none) { \
 			code;			\
 		};
 
-#define REMOVE_WARNS pi=val.get.constant;pi++;pi=a[0].get.constant; /* a silly line just to remove annoying "unused" warnings*/
-#define HANDLE_ERROR if (*err!=0) return NULL;
 
+#define REMOVE_WARNS pi=val.get.constant;pi++;pi=a[0].get.constant; /* a silly line just to remove annoying "unused" warnings*/
+#define HANDLE_ERROR if (*err!=0) return NULL; /* in case our commands hit errors, lets make sure we return null.. */
+
+/* This section will define the OR# structures. i didnt manage to compose OR well to be able to say something like
+ *   #define OR3 OR(A, OR2(B,C))
+ * because i had to deal explicitly with retrieving back the chosen values. perhaps a bit more time on this would
+ * enable me to do so..
+ * */
+
+/* OR2, makes sure only ONE of either A or B succeeds */
 #define OR2(A,B)   \
     /*OR2*/ 		    \
     DO_(2)		        \
@@ -38,6 +52,7 @@
 	A;B;				\
 	l = NULL;
 
+/* OR2, makes sure only ONE of either A, B or C succeeds */
 #define OR3(A,B,C) \
     /*OR3*/ 		    \
     DO_(3)		        \
@@ -46,21 +61,38 @@
 	l = NULL;
 
 #define OR4(A,B,C,D)   \
-    /*OR3*/ 		    \
+    /*OR4*/ 		    \
     DO_(4)		        \
     l = NULL;			\
 	A;B;C;D;			\
 	l = NULL;
 
 #define OR5(A,B,C,D,E)   \
-    /*OR3*/ 		    \
+    /*OR5*/ 		    \
     DO_(5)		        \
     l = NULL;			\
 	A;B;C;D;E;			\
 	l = NULL;
 
+/* a convenient macro for getting the wanted value */
 #define VAL(i) theVals[i-1]
 
+/* makes sure that we have no unneeded operands and parameters*/
+#define VERIFY { \
+	if (*p!='\n' && *p!='\0' && *p!='\r') { \
+		*err=COMMAND_NOT_TERMINATED; \
+		return NULL; \
+	} \
+}
+
+/*
+ * In this section we define all the specific parsers, generally composed of our parser utility.
+ *
+ * This is the main building block of our DSL:
+ *
+ * */
+
+/* DIRECT - this macro is responsible for parsing a direct label */
 #define DIRECT \
 	if (l==NULL) /*DIRECT*/ { \
 		l = strip(getAllAlphasDigits(strip(p," "), a[pi].get.direct), " "); \
@@ -73,6 +105,7 @@
 	}\
 	pi++;
 
+/* LABEL1 - this macro is responsible of parsing a label with a single, relative label as the index */
 #define LABEL1 \
 	if (l==NULL) /*LABEL1*/ { \
 		l = charIs(getAllAlphasDigits(charIs(charIs(getAllAlphasDigits(strip(p," "), a[pi].get.oneIndex.label), '[') ,'%'), a[pi].get.oneIndex.index), ']'); \
@@ -85,6 +118,7 @@
 	}\
 	pi++;
 
+/* LABEL2 - this macro is responsible of parsing a label with a two indices, the first one a label, and the other a register */
 #define LABEL2 \
 	if (l==NULL) /*LABEL2*/ { \
 		char r; \
@@ -101,6 +135,7 @@
 	}\
 	pi++;
 
+/* REG - this macro is responsible of parsing a register */
 #define REG  \
 	if (l==NULL) /*REG*/ { \
 		char* n; \
@@ -118,7 +153,7 @@
 	}\
 	pi++;
 
-
+/* CONST - this macro is responsible of parsing a numeric value */
 #define CONST  \
 	if (l==NULL) /*CONST*/ { \
 		l = charIs(strip(p," "),'#');\
@@ -133,13 +168,13 @@
 	}\
     pi++;
 
-#define VERIFY { \
-	if (*p!='\n' && *p!='\0' && *p!='\r') { \
-		*err=COMMAND_NOT_TERMINATED; \
-		return NULL; \
-	} \
-}
+/*
+ * In this section we define the mechanics that glues our operand matching to our generating functions
+ *
+ * This too is a main feature of our dsl.
+ * */
 
+/* TWO - define an instruction that takes in two parameters (or operands) */
 #define TWO(A,B,code) { \
 	Operand theVals[2],val; /*TWO*/	\
 	Operand a[2];\
@@ -167,6 +202,7 @@
 	return p; \
 }
 
+/* ONE - define an instruction that takes in only a single parameter (or operand) */
 #define ONE(A, code) { \
 		Operand theVals[1],val; /*ONE*/	\
 		Operand a[1];\
@@ -187,8 +223,14 @@
 		return p; \
 }
 
+/* NONE - define an instruction that takes no parameters (or operands) */
 #define NONE(code) {if (p==NULL) return NULL; VERIFY; code; return p;}
 
+/*
+ *  This section is for the more specific case of the dot featurs, like .data and .string
+ *  */
+
+/* this one is used by .data, as it parses the input for multiple numbers, and calls code which should save the result in the AST */
 #define MANY_NUMBERS(code) { \
 	char* l; \
 	int* nums;\
@@ -219,6 +261,8 @@
 	return *err!=0?NULL:p;\
 }
 
+/* this one is used by .string. it will make sure that string is wraped with " on both ends,
+ * and call code which stores that string in the ast */
 #define STRING(code) { \
 	char* text,*l; \
 	if (p==NULL) return NULL; \
@@ -234,11 +278,18 @@
 	return p; \
 }
 
+/*
+ * The following section will define everything needed for the main procedure of the application,
+ * openeing files, reading lines, reading labels, parsing ,trying out various parsers and generating output files.
+ * */
+
+/* a macro used to strip a label out of a line, so we dont have to code that into each parser */
 #define GET_LINE_LABEL { \
 	char* o=line; \
 	if(!(isAlpha(line) && (l=charIs(getAllAlphasDigits(line, label), ':')))) { label[0]='\0'; l=o;} \
 }
 
+/* the entry point to the parsers */
 #define PARSE  { \
 	int err=0,len; \
 	Label label;\
@@ -253,22 +304,30 @@
 	}\
 	if(oneOf(l, "\n\r;")) {}
 
+/* The following macros:
+ * TRY: attempt a parser by the name cmd, if it fails, it moves on to the next parser
+ * TRY_DOT: attempt a parser for the dot commands, such as .extern, .string etc.. notice the use of or in there. that is because extern is a reserved word in c
+ * ELSE: in case of an error, print out an error messages, and mark this input stream as errornous, so no output will get generated.
+ * */
 #define TRY(cmd)      else if (cmd (matchWordD(l, #cmd) , &context, &err, label, lineCounter, line)) {}
 #define TRY_DOT(cmd)  else if (cmd (matchWordD(charIs(l, '.'), or(charIs(#cmd,'_'),#cmd)), &context, &err, label,lineCounter, line)) {}
 #define ELSE(msg)     else if (!err){ printf(" Error at line %d '%s': %s\n",lineCounter,trimNewline(line),  msg);  dontGenerate = 1;}\
 					  else if (err) { \
 						  dontGenerate = 1;\
 						  switch(err) { \
-						  	  case FIRST_ARG_IS_INVALID: printf(" Error at line %d '%s': first argument is not valid\n", lineCounter, trimNewline(line));break; \
-						  	  case SECOND_ARG_IS_INVALID:printf(" Error at line %d '%s': second argument is not valid\n",lineCounter,  trimNewline(line));break; \
-						  	  case COMMAND_NOT_TERMINATED:printf(" Error at line %d '%s': command has extra parameters\n", lineCounter,  trimNewline(line));break; \
-						  	  case EMPTY_DATA:printf(" Error at line %d '%s': data is empty\n", lineCounter,  trimNewline(line));break; \
-						  	  case BAD_DATA: printf(" Error at line %d '%s': data needs another number\n", lineCounter,  trimNewline(line));break; \
+						  	  case FIRST_ARG_IS_INVALID: 	printf(" Error at line %d '%s': first argument is not valid\n", lineCounter, trimNewline(line));break; \
+						  	  case SECOND_ARG_IS_INVALID:	printf(" Error at line %d '%s': second argument is not valid\n",lineCounter,  trimNewline(line));break; \
+						  	  case COMMAND_NOT_TERMINATED:	printf(" Error at line %d '%s': command has extra parameters\n", lineCounter,  trimNewline(line));break; \
+						  	  case EMPTY_DATA:				printf(" Error at line %d '%s': data is empty\n", lineCounter,  trimNewline(line));break; \
+						  	  case BAD_DATA: 				printf(" Error at line %d '%s': data needs another number\n", lineCounter,  trimNewline(line));break; \
 						  } \
 					  }}
 
+/* this macro will run for each line in a file */
 #define FOREACH_LINE(parse) while (fgets(line, MAXLINE-1, fr) != NULL) {parse;}
 
+/* this macro will run doWithFile on each file given to the application
+ * baseAddress is the base address for the code generation. usually 100.*/
 #define	FOREACH_FILE(baseAddress, doWithFile) \
 	int i; \
 	for (i = 1; i < argc; i++) { \
@@ -334,16 +393,14 @@
 		}\
 	}
 
-
-void func(Operand oper1, Operand oper2, Label label);
-void debugPrint(Operand oper);
-
+/* The set of defines here are used to call a generation function from within a parser declaration */
 #define GEN2(gen)		 gen##_gen(context, VAL(1), VAL(2), label, err, lineNumber,originalLine)
 #define GEN1(gen) 		 gen##_gen(context, VAL(1), label, err,lineNumber,originalLine)
 #define GEN0(gen) 		 gen##_gen(context, label, err, lineNumber,originalLine)
 #define CALL_DATA(gen)   gen##_gen(context, label, err, nums, count, lineNumber,originalLine)
 #define CALL_STRING(gen) gen##_gen(context, label, err, text, lineNumber,originalLine)
 
+/* This section assign each ASM command with its proper op-code, and the number of operands it has */
 #define MOV(OP1, OP2) OPER2(0,OP1,OP2)
 #define CMP(OP1, OP2) OPER2(1,OP1,OP2)
 #define ADD(OP1, OP2) OPER2(2,OP1,OP2)
@@ -363,31 +420,31 @@ void debugPrint(Operand oper);
 #define RTS  OPER0(14)
 #define STOP OPER0(15)
 
-#define CMD(name) char* name(char* p, Context* context, int* err, char* label, int lineNumber, char* originalLine)
+/* defines what a parser function looks like*/
+#define PARSER(name) char* name(char* p, Context* context, int* err, char* label, int lineNumber, char* originalLine)
 
-CMD(data);
+/* and now lets forward declare all the parsers we will be using, so main could access them */
+PARSER(cmp);
+PARSER(mov);
+PARSER(add);
+PARSER(sub);
+PARSER(lea);
 
-CMD(cmp);
-CMD(mov);
-CMD(add);
-CMD(sub);
-CMD(lea);
+PARSER(prn);
+PARSER(not);
+PARSER(clr);
+PARSER(inc);
+PARSER(dec);
+PARSER(jmp);
+PARSER(bne);
+PARSER(red);
+PARSER(jsr);
+PARSER(rts);
+PARSER(stop);
 
-CMD(prn);
-CMD(not);
-CMD(clr);
-CMD(inc);
-CMD(dec);
-CMD(jmp);
-CMD(bne);
-CMD(red);
-CMD(jsr);
-CMD(rts);
-CMD(stop);
-
-CMD(_extern);
-CMD(entry);
-CMD(data);
-CMD(string);
+PARSER(_extern);
+PARSER(entry);
+PARSER(data);
+PARSER(string);
 
 #endif /* ASMDSL_H_ */
